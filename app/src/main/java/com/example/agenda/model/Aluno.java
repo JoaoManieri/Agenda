971b8669ac59c4600cd58e88fa82b40665ade7e5
João Manieri <71887875+JoaoManieri@users.noreply.github.com>
2022-05-04 10:59:58 -0300

    Stable release.

--- app/src/main/java/com/example/agenda/model/Aluno.java
+++ app/src/main/java/com/example/agenda/model/Aluno.java
@@ -6,21 +6,14 @@ public class Aluno {
     private final String telefone;
     private final String email;
 
-    public String getNome() {
-        return nome;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
     public Aluno(String nome, String telefone, String email) {
         this.nome = nome;
         this.telefone = telefone;
         this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
     }
 }
--- app/src/main/java/com/example/agenda/ui/acyivity/ListaAlunosActivity.java
+++ app/src/main/java/com/example/agenda/ui/acyivity/ListaAlunosActivity.java
@@ -1,13 +1,19 @@
 package com.example.agenda.ui.acyivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.service.controls.actions.FloatAction;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.agenda.R;
+import com.example.agenda.dao.AlunoDAO;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,13 +25,31 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+
+
         setTitle("Lista de alunos");
-        List<String> alunos = new ArrayList<>(
-                Arrays.asList("Alex","Joao","Jose","Belatrix","Eugenio"));
+
+        FloatingActionButton fabButton = findViewById(R.id.activity_lista_alunos_fab_novo_aluno);
+
+        fabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ListaAlunosActivity.this, FormularioAlunoActivity.class));
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        AlunoDAO dao = new AlunoDAO();
+
         ListView listaDeAlnos = findViewById(R.id.activity_main_lista_alunos_listview);
         listaDeAlnos.setAdapter(new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
-                alunos));
+                dao.todos()));
     }
 }
